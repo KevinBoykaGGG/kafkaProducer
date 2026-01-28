84c6305a66c04600d756f30bd55d69c2363643a9
KevinBoykaGGG <kevinalex360boyka@gmail.com>
2026-01-28 16:34:07 -0600

    no changes

--- src/main/java/com/lta/backend/str_producer/StrProducerApplication.java
+++ src/main/java/com/lta/backend/str_producer/StrProducerApplication.java
@@ -3,6 +3,7 @@ package com.lta.backend.str_producer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class StrProducerApplication {
 
